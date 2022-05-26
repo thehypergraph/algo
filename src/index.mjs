0de1682f66caf78ff1ac8c2a0fe9bc7475a72c2f
Ronald Jordan <amisword@gmail.com>
2022-05-26 22:43:17 +0200

    Add exports

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,6 @@
 import Queue from './data/Queue.mjs'
+import Stack from './data/Stack.mjs'
 import Fib from './algo/Fib.mjs'
 import Factorial from './algo/Factorial.mjs'
 
-export { Queue, Fib, Factorial }
+export { Queue, Stack, Fib, Factorial }
